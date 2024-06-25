d22e53e6e5327b3500d12d6dd9f5332de6eb583f
SawanPanwar <skpanwar25@gmail.com>
2024-06-25 12:11:24 +0530

    done

--- django-project-simple/SOS/ORS/urls.py
+++ django-project-simple/SOS/ORS/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('test/', views.test),
     path('register/', views.register_user),
+    path('testList/', views.test_list),
+    path('signIn/', views.user_signin),
+    path('welcome/', views.welcome),
 ]
--- django-project-simple/SOS/ORS/views.py
+++ django-project-simple/SOS/ORS/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .service.UserService import UserService
 
 
 def test(request):
@@ -8,17 +10,44 @@ def test(request):
 
 
 def register_user(request):
+    message = ''
     if request.method == "POST":
-        firstName = request.POST.get('firstName')
-        lastName = request.POST.get('lastName')
+        params = {}
+        params['firstName'] = request.POST.get('firstName')
+        params['lastName'] = request.POST.get('lastName')
+        params['loginId'] = request.POST.get('loginId')
+        params['password'] = request.POST.get('password')
+        params['dob'] = request.POST.get('dob')
+        params['address'] = request.POST.get('address')
+        service = UserService()
+        service.add(params)
+        message = 'User Registered Successfully'
+    return render(request, 'UserRegistration.html', {'message': message})
+
+
+def user_signin(request):
+    message = ''
+    if request.method == "POST":
         loginId = request.POST.get('loginId')
         password = request.POST.get('password')
-        dob = request.POST.get('dob')
-        address = request.POST.get('address')
-        print(firstName)
-        print(lastName)
-        print(loginId)
-        print(password)
-        print(dob)
-        print(address)
-    return render(request, 'UserRegistration.html')
+        service = UserService()
+        user_data = service.auth(loginId, password)
+        if len(user_data) != 0:
+            # request.session["firstName"] = user_data[0].get('firstName')
+            return redirect("/ORS/welcome")
+        else:
+            message = 'login id & password is invalid'
+    return render(request, "Login.html", {'message': message})
+
+
+def welcome(request):
+    return render(request, "Welcome.html")
+
+
+def test_list(request):
+    list = [
+        {"id": 1, "firstName": "abc", "lastName": "aaa", "email": "abc@gmail.com", "password": "12345"},
+        {"id": 2, "firstName": "xyz", "lastName": "aaa", "email": "abc@gmail.com", "password": "12345"},
+        {"id": 3, "firstName": "pqr", "lastName": "aaa", "email": "abc@gmail.com", "password": "12345"}
+    ]
+    return render(request, "TestList.html", {"list": list})

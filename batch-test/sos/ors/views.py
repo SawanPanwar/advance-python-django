54601bcc212f839b8182dd0890851457c2a047b4
SawanPanwar <skpanwar25@gmail.com>
2026-03-16 13:21:52 +0530

    16-03-2026

--- batch-test/sos/ors/views.py
+++ batch-test/sos/ors/views.py
@@ -10,6 +10,7 @@ def test_ors(request):
 def welcome(request):
     return render(request, 'welcome.html')
 
+
 def user_signup_test(request):
     print(request.GET.get('firstName'))
     print(request.GET.get('lastName'))
@@ -18,6 +19,7 @@ def user_signup_test(request):
     print(request.GET.get('dob'))
     print(request.GET.get('address'))
     return render(request, 'registration.html')
+
 
 def user_signup(request):
     if request.method == "POST":
@@ -32,11 +34,20 @@ def user_signup(request):
         service.add(params)
     return render(request, 'registration.html')
 
+
 def user_signin(request):
+    message = ''
     if request.method == "POST":
         if request.POST.get('operation') == "signIn":
-            print(request.POST.get('loginId'))
-            print(request.POST.get('password'))
+            loginId = request.POST.get('loginId')
+            password = request.POST.get('password')
+            service = UserService()
+            user_data = service.auth(loginId, password)
+            if len(user_data) != 0:
+                # return redirect('/ors/welcome')
+                return render(request, 'welcome.html', {'firstName': user_data[0].get('firstName')})
+            else:
+                message = 'Login ID & Password Invalid'
         if request.POST.get('operation') == "signUp":
             return redirect("/ors/signup/")
-    return render(request, 'login.html')
+    return render(request, 'login.html', {'message': message})

e8b5eb5891ab217743ccbb062e4a4f6333a0d425
SawanPanwar <skpanwar25@gmail.com>
2026-03-25 13:06:40 +0530

    25-03-2026

--- batch-test-02/sos/ors/urls.py
+++ batch-test-02/sos/ors/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('signup/', views.user_signup),
     path('signin/', views.user_signin),
     path('logout/', views.user_logout),
+    path('save/', views.add_marksheet),
+    path('list/', views.marksheet_list),
+    path('delete/<int:id>/', views.delete_marksheet),
+    path('edit/<int:id>/', views.edit_marksheet),
 ]
--- batch-test-02/sos/ors/views.py
+++ batch-test-02/sos/ors/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
+from .models import Marksheet
+
 
 def welcome(request):
     return render(request, 'welcome.html')
@@ -44,4 +46,35 @@ def user_signin(request):
 def user_logout(request):
     logout(request)
     return redirect('/ors/signin/')
+
+
+def add_marksheet(request):
+    message = ''
+    if request.method == "POST":
+        marksheet = Marksheet()
+        marksheet.rollNo = request.POST["rollNo"]
+        marksheet.name = request.POST["name"]
+        marksheet.physics = request.POST["physics"]
+        marksheet.chemistry = request.POST["chemistry"]
+        marksheet.maths = request.POST["maths"]
+        marksheet.save()
+
+        message = 'Marksheet Added Successfully'
+    return render(request, "marksheet.html", {'message': message})
+
+
+def marksheet_list(request):
+    list = Marksheet.objects.all()
+    return render(request, "marksheetlist.html", {"list": list})
+
+
+def delete_marksheet(request, id):
+    obj = Marksheet.objects.get(id=id)
+    obj.delete()
+    return redirect("/ors/list/")
+
+def edit_marksheet(request, id):
+    message = ''
+    obj = Marksheet.objects.get(id=id)
+    return render(request, "marksheet.html", {"form": obj, "id": id, 'message': message})
 

b7e370af6acdfe33f52bfb932dbc6d84e3a79313
SawanPanwar <skpanwar25@gmail.com>
2026-07-23 10:43:40 +0530

    delete

--- flask-ors/app.py
+++
@@ -1,6 +0,0 @@
-from ors import create_app
-
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
--- flask-ors/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SECRET_KEY = "123456"
-
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost:3306/flask_ors"
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False

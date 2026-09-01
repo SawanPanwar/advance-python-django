06142d86e0d87bdf5069b1a5f982c1ceefa3c438
SawanPanwar <skpanwar25@gmail.com>
2026-09-01 17:03:22 +0530

    01 Sept

--- aug-batch/sos/ors/views.py
+++ aug-batch/sos/ors/views.py
@@ -17,7 +17,16 @@ def welcome(request):
 
 
 def user_signup(request):
+    print(request.GET.get('firstName'))
+    print(request.GET.get('lastName'))
+    print(request.GET.get('loginId'))
+    print(request.GET.get('password'))
+    print(request.GET.get('dob'))
+    print(request.GET.get('address'))
     return render(request, 'registration.html')
 
+
 def user_signin(request):
+    print(request.GET.get('loginId'))
+    print(request.GET.get('password'))
     return render(request, 'login.html')
